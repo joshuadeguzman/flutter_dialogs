c759c2f5bdfe54a87e7d7952249d323d850d8717
jdeguzman <jdeguzman@freelancer.com>
2020-01-21 19:16:31 +0800

    Update SDK

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/joshuadeguzman/Documents/Tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/joshuadeguzman/Documents/github/flutter-dialogs/example"
+export "FLUTTER_APPLICATION_PATH=/Users/joshuadeguzman/Desktop/flutter_dialogs/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
